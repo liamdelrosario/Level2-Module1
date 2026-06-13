87bd375a1fd0a7156f9f755aec7cd130d3d19474
league <league@192.168.1.85>
2026-06-13 16:28:55 -0700

    pretty good amount of progress

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -7,30 +7,40 @@ public class IntroToArrayLists {
 		//   Don't forget to import the ArrayList (java.util) class
 		ArrayList<String> str = new ArrayList<String>();
 		//2. Add five Strings to your list
-		str.add("Never");
-		str.add("Trust");
-		str.add("A");
-		str.add("Soul");
-		str.add("In");
+		str.add("...apes..");
+		str.add(",,,brock,,");
+		str.add("'''cobalt''");
+		str.add("|||drought||");
+		str.add(":::electric::");
 
 		//3. Print all the Strings using a standard for-loop
-	/*	for(int i=0; i<str.size(); i++) {
+		for(int i=0; i<str.size(); i++) {
 			String s = str.get(i);
 			System.out.println(s);
 		}
-		for(int i=0; i<list.size(); i++){
-		String s = list.get(i);
-		}
-		for(String s : list){
-		syso(s);
-		}
-		*/
+
 		//4. Print all the Strings using a for-each loop
-		
+		for(String seq: str) {
+			System.out.println(seq);
+		}		
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i=1; i<str.size(); i+=2) {
+		String s = str.get(i);
+		System.out.println(s);
+	}
 		//6. Print all the Strings in reverse order.
-		
+		for(int i=4; i>=0; i-=1) {
+			String s = str.get(i);
+			System.out.println(s);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for(int i=0; i<str.size(); i++) {
+			
+			String s = str.get(i);
+			
+			if(s.contains("e")) {
+			System.out.println(s);
+			}
+		}
 	}
 }
--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,16 +16,26 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
+		list.add("Dread");
+		list.add("Escuse");
+		list.add("Ionization");
+		list.add("Flavinium");
+		list.add("Compensating");
 		
 		// 2. Set one item in our list to be something different
-		
+		list.set(1, "Jargonistically");
 		// 3. Remove something from our list
-		
+		list.remove(4);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		String s = list.get(3);
+		if(s.equals("Flavinum")) {
+			System.out.println("exist");
+		}else {
+			System.out.println("Non-existent");
+		}
 		// 6. Get a specific item from our list and print it out
 		
 		// 7. Clear our list of all items
