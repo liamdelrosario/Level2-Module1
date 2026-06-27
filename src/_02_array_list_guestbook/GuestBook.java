1eca88a85b43d38debd3ec03dd1dd8d7a683fa49
league <league@192.168.1.87>
2026-06-27 14:27:59 -0700

    hello there I was kind of locking in

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,10 +1,18 @@
 package _02_array_list_guestbook;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import _01_array_list_visualizer.data.ArrayList;
+
 import javax.swing.JLabel;
-public class GuestBook {
+import javax.swing.JOptionPane;
+public class GuestBook implements ActionListener {
 	JButton addName; 
+	JButton viewNames; 
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -13,16 +21,41 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> list = new ArrayList<String>();
 	
 	JPanel panel = new JPanel();
 	public void run() {
+		
 		JFrame frame = new JFrame("");
 		frame.add(panel);
 		frame.setSize(580, 420);
+		addName =new JButton("Add Name");
+		viewNames = new JButton("View Names");
+		panel.add(addName);
+		panel.add(viewNames);
+		addName.addActionListener(this);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
 		new GuestBook().run();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+		JButton buttonPressed = (JButton) e.getSource();	
+		if(addName==buttonPressed) {
+			System.out.println("Yeah I did wrote it in Github");
+			String input = JOptionPane.showInputDialog("Enter a new ");
+			list.add(input);
+			for(int i=0; i<list.size(); i++) {
+				String s= list.get(i);
+				System.out.println(s);
+			}
+		}
+		if(viewNames==buttonPressed) {
+			
+		}
 	}
 }
